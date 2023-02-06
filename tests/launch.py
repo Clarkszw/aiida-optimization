36f436845c1cce27c96e5f7162532386492636bb
Clark <clarkszw@gmail.com>
2023-02-06 15:01:53 +0000

    before the first trail calculation after the implementation

--- tests/launch.py
+++ tests/launch.py
@@ -5,27 +5,30 @@ from pathlib import Path
 from aiida import engine, orm
 from aiida.common.exceptions import NotExistent
 
-INPUT_DIR = Path(__file__).resolve().parent / 'input_files'
+#INPUT_DIR = Path(__file__).resolve().parent / 'input_files'
+INPUT_DIR = Path(__file__).resolve().parent
 
 # Create or load code
 computer = orm.load_computer('localhost')
 try:
-    code = orm.load_code('diff@localhost')
+    code = orm.load_code('opt@localhost')
 except NotExistent:
     # Setting up code via python API (or use "verdi code setup")
-    code = orm.Code(label='diff', remote_computer_exec=[computer, '/usr/bin/diff'], input_plugin_name='diff-tutorial')
+    code = orm.Code(label='opt', remote_computer_exec=[computer, '/home/clarkszw/envs/aiida/bin/python3'], input_plugin_name='optimization')
 
 # Set up inputs
 builder = code.get_builder()
-builder.file1 = orm.SinglefileData(file=INPUT_DIR / 'file1.txt')
-builder.file2 = orm.SinglefileData(file=INPUT_DIR / 'file2.txt')
-builder.metadata.description = 'Test job submission with the aiida_diff_tutorial plugin'
+builder.atom = orm.SinglefileData(file=INPUT_DIR / 'N2')
+# String = orm.DataFactory("string")
+# builder.basis = String(input_string = "ccpvdz").store()
+builder.basis = Str("ccpvdz")
+builder.metadata.description = 'Test job submission with the optimization plugin'
 
 # Run the calculation & parse results
 result = engine.run(builder)
-computed_diff = result['diff'].get_content()
-print(f'Computed diff between files:\n{computed_diff}')
+optimized = result['opt'].get_content()
+print(f'The optimized structure is::\n{optimized}')
 
 # Submit calculation to the aiida daemon
-node = engine.submit(builder)
-print("Submitted calculation {}".format(node))
+# node = engine.submit(builder)
+# print("Submitted calculation {}".format(node))
